e5d3db5c194030990dd6ef77530ed3774d3811cf
luizgustavz <luiz.gus.ms@gmail.com>
2025-08-13 13:31:16 -0300

    first commit

---
+++ src/main/java/com/luizgustavz/character/character/api/springboot/CharacterApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.luizgustavz.character.character.api.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizgustavz/character/character/api/springboot/CharacterApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizgustavz.character.character.api.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
